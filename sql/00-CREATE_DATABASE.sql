89ec648dac617af5028bb916c94f337b6cd1c815
Carlos Eduardo Pauluk <carlospauluk@gmail.com>
2020-04-27 18:32:18 -0300

    ...

---
+++ sql/00-CREATE_DATABASE.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE `crosier_equilibre_dev` CHARACTER SET 'utf8' COLLATE 'utf8_swedish_ci';
+GRANT ALL PRIVILEGES ON crosier_equilibre_dev.* TO 'crosier_equilibre_dev'@'localhost' IDENTIFIED BY 'crosier_equilibre_dev';
+FLUSH PRIVILEGES;
